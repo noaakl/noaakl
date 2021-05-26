138d17c91dbbc65166fc213f4da301b7dd4a32f1
idohersko <idohersko@gmail.com>
2021-05-26 15:03:42 +0300

    "About page" reviewed and finished after remarks

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,11 +6,11 @@ const About = () => {
       <img src="https://user-images.githubusercontent.com/16198896/114301821-0653e000-9acf-11eb-923d-baeec7ba80dc.png" alt="new"/>
       <h1>map-my-shifter</h1>
       <h3>STIX-Shifter Connector's Mapping Builder</h3>
-      <Accordion>
+      <Accordion align='start'>
 
-          <AccordionItem title="Introduction">
+          <AccordionItem title="Introduction" open>
               The map-my-shifter (MMS) project provides a visual way for building mapping files for <a href={"https://github.com/opencybersecurityalliance/stix-shifter"}>STIX-Shifter</a> project.
-              <ul Class="firstUl">
+              <ul className="first-ul">
                   <li>
                       - From STIX pattern mapping - When building the data source query from STIX query, the STIX fields, for examples `file:name`, is mapped to the target data source's field. <a href={"https://github.com/opencybersecurityalliance/stix-shifter/blob/master/adapter-guide/develop-translation-module.md#step-2-edit-the-from_stix_map-json-files"}>Read more...</a>
                   </li>
@@ -19,8 +19,8 @@ const About = () => {
                   </li>
               </ul>
           </AccordionItem>
-          <AccordionItem title="Use-cases">
-              <ul Class="secondUl">
+          <AccordionItem title="Use-cases" open>
+              <ul className="second-ul">
                   <li>
                       MMS is a static client side app, there is no backend involved, except from serving the static content. It is built with <a href={"https://reactjs.org"}>ReactJS</a> library, and designed using <a href={"https://www.carbondesignsystem.com"}>Carbon Design System</a> components.
                   </li>
@@ -29,8 +29,8 @@ const About = () => {
 
               </p>
           </AccordionItem>
-          <AccordionItem title="Devlopment">
-              <ul Class="secondUl">
+          <AccordionItem title="Devlopment" open>
+              <ul className="second-ul">
                   <li>
                       - Create mapping file from scratch.
                   </li>
@@ -39,8 +39,8 @@ const About = () => {
                   </li>
               </ul>
           </AccordionItem>
-          <AccordionItem title="Authors">
-              <ul Class="secondUl">
+          <AccordionItem title="Authors" open>
+              <ul className="second-ul">
                   <li>
                       <a href={"https://github.com/barvhaim"}>Bar Haim</a>
                   </li>
@@ -49,16 +49,17 @@ const About = () => {
                   </li>
               </ul>
           </AccordionItem>
-          <AccordionItem title="Licensing">
-              <ul Class="secondUl">
+          <AccordionItem title="Licensing" open>
+              <ul className="second-ul">
                   <li>
                       map-my-shifter is licensed under the Apache License, Version 2.0. See  <a href={"https://github.com/barvhaim/map-my-shifter/blob/master/LICENSE"}>LICENSE</a> for the full license text.
                   </li>
               </ul>
           </AccordionItem>
       </Accordion>
-      <p Class="firstP">
-          Built with ❤️ from <a href={"https://www.research.ibm.com/haifa/ccoe/"}>IBM Cyber Security Center of Excellence (CCoE)</a>
+
+      <p className="first-p">
+          Built with <span className="heart">❤</span> from <a href={"https://www.research.ibm.com/haifa/ccoe/"}>IBM Cyber Security Center of Excellence (CCoE)</a>
       </p>
 
   </div>;
