42fa197054ce62e0d1274137718592b59c179449
Bar Haim <barha@il.ibm.com>
2021-06-06 13:30:02 +0300

    About page styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,68 +1,122 @@
 import React from "react";
-import {Accordion, AccordionItem} from "carbon-components-react";
-import './About.css'
+import {
+  Accordion,
+  AccordionItem,
+  UnorderedList,
+  ListItem,
+} from "carbon-components-react";
+import "./About.scss";
+
 const About = () => {
-  return<div>
-      <img src="https://user-images.githubusercontent.com/16198896/114301821-0653e000-9acf-11eb-923d-baeec7ba80dc.png" alt="new"/>
-      <h1>map-my-shifter</h1>
-      <h3>STIX-Shifter Connector's Mapping Builder</h3>
-      <Accordion align='start'>
+  return (
+    <>
+      <div className={"bx--row"}>
+        <div className={"bx--col"}>
+          <img
+            src="https://user-images.githubusercontent.com/16198896/114301821-0653e000-9acf-11eb-923d-baeec7ba80dc.png"
+            alt="logo"
+          />
+          <h4>STIX-Shifter Connector's Mapping Builder</h4>
+        </div>
+      </div>
 
-          <AccordionItem title="Introduction" open>
-              The map-my-shifter (MMS) project provides a visual way for building mapping files for <a href={"https://github.com/opencybersecurityalliance/stix-shifter"}>STIX-Shifter</a> project.
-              <ul className="first-ul">
-                  <li>
-                      - From STIX pattern mapping - When building the data source query from STIX query, the STIX fields, for examples `file:name`, is mapped to the target data source's field. <a href={"https://github.com/opencybersecurityalliance/stix-shifter/blob/master/adapter-guide/develop-translation-module.md#step-2-edit-the-from_stix_map-json-files"}>Read more...</a>
-                  </li>
-                  <li>
-                      - To STIX object mapping - When results object is back from the data source, this object should be displayed in the final results as STIX object. For examples {'{'}"filename": "xxxxx"}  should be translated to STIX object of type `file`. <a href={"https://github.com/opencybersecurityalliance/stix-shifter/blob/master/adapter-guide/develop-translation-module.md#step-4-edit-the-to_stix_map-json-file"}>Read more...</a>
-                  </li>
-              </ul>
-          </AccordionItem>
-          <AccordionItem title="Use-cases" open>
-              <ul className="second-ul">
-                  <li>
-                      MMS is a static client side app, there is no backend involved, except from serving the static content. It is built with <a href={"https://reactjs.org"}>ReactJS</a> library, and designed using <a href={"https://www.carbondesignsystem.com"}>Carbon Design System</a> components.
-                  </li>
-              </ul>
+      <div className={"bx--row"}>
+        <div className={"bx--col"}>
+          <Accordion align="start">
+            <AccordionItem title="Introduction" open>
               <p>
-
+                The map-my-shifter (MMS) project provides a visual way for
+                building mapping files for{" "}
+                <a
+                  href={
+                    "https://github.com/opencybersecurityalliance/stix-shifter"
+                  }
+                >
+                  STIX-Shifter
+                </a>{" "}
+                project.
               </p>
-          </AccordionItem>
-          <AccordionItem title="Devlopment" open>
-              <ul className="second-ul">
-                  <li>
-                      - Create mapping file from scratch.
-                  </li>
-                  <li>
-                      - Load existing mapping file, edit the file and save it to a new file.
-                  </li>
-              </ul>
-          </AccordionItem>
-          <AccordionItem title="Authors" open>
-              <ul className="second-ul">
-                  <li>
-                      <a href={"https://github.com/barvhaim"}>Bar Haim</a>
-                  </li>
-                  <li>
-                      <a href={"https://github.com/idohersko"}>Ido Hersko</a>
-                  </li>
-              </ul>
-          </AccordionItem>
-          <AccordionItem title="Licensing" open>
-              <ul className="second-ul">
-                  <li>
-                      map-my-shifter is licensed under the Apache License, Version 2.0. See  <a href={"https://github.com/barvhaim/map-my-shifter/blob/master/LICENSE"}>LICENSE</a> for the full license text.
-                  </li>
-              </ul>
-          </AccordionItem>
-      </Accordion>
 
-      <p className="first-p">
-          Built with <span className="heart">❤</span> from <a href={"https://www.research.ibm.com/haifa/ccoe/"}>IBM Cyber Security Center of Excellence (CCoE)</a>
-      </p>
+              <UnorderedList>
+                <ListItem>
+                  From STIX pattern mapping - When building the data source
+                  query from STIX query, the STIX fields, for examples
+                  `file:name`, is mapped to the target data source's field.
+                  <a
+                    href={
+                      "https://github.com/opencybersecurityalliance/stix-shifter/blob/master/adapter-guide/develop-translation-module.md#step-2-edit-the-from_stix_map-json-files"
+                    }
+                  >
+                    Read more...
+                  </a>
+                </ListItem>
+                <ListItem>
+                  To STIX object mapping - When results object is back from the
+                  data source, this object should be displayed in the final
+                  results as STIX object. For examples {"{"}"filename": "xxxxx"}
+                  should be translated to STIX object of type `file`.
+                  <a
+                    href={
+                      "https://github.com/opencybersecurityalliance/stix-shifter/blob/master/adapter-guide/develop-translation-module.md#step-4-edit-the-to_stix_map-json-file"
+                    }
+                  >
+                    Read more...
+                  </a>
+                </ListItem>
+              </UnorderedList>
+            </AccordionItem>
 
-  </div>;
+            <AccordionItem title="Use-cases" open>
+              <UnorderedList>
+                <ListItem>Create mapping file from scratch.</ListItem>
+                <ListItem>
+                  Load existing mapping file, edit the file and save it to a new
+                  file
+                </ListItem>
+              </UnorderedList>
+            </AccordionItem>
+
+            <AccordionItem title="Authors" open>
+              <UnorderedList>
+                <ListItem>
+                  <a href={"https://github.com/barvhaim"}>Bar Haim</a>
+                </ListItem>
+                <ListItem>
+                  <a href={"https://github.com/idohersko"}>Ido Hersko</a>
+                </ListItem>
+              </UnorderedList>
+            </AccordionItem>
+
+            <AccordionItem title="Licensing" open>
+              <p>
+                map-my-shifter is licensed under the Apache License, Version
+                2.0. See{" "}
+                <a
+                  href={
+                    "https://github.com/barvhaim/map-my-shifter/blob/master/LICENSE"
+                  }
+                >
+                  LICENSE
+                </a>{" "}
+                for the full license text.
+              </p>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
+
+      <div className={"bx--row"}>
+        <div className={"bx--col footer"}>
+          <p>
+            Built with <span className="heart">❤</span> from{" "}
+            <a href={"https://www.research.ibm.com/haifa/ccoe/"}>
+              IBM Cyber Security Center of Excellence (CCoE)
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
